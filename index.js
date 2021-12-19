9614414c80fb61e00eab643170204ef0cb4f6753
LucyMauger <92333725+LucyMauger@users.noreply.github.com>
2021-12-19 23:15:26 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+
+var chart = new Chart(ctx, {
+  type: 'bar',
+  plugins: [ChartDataSource],
+  options: {
+    daatasource: {
+      url: 'GoogleTrendsComparingDesigners.csv'
+    }
+  }
+});
