f1fb50ea378cc90e37b77710e65255d2016af4f5
LucyMauger <LucyMauger@users.noreply.github.com>
2021-12-20 03:09:25 +0000

    i

--- script.js
+++ script.js
@@ -1,54 +1,54 @@
 d3.csv("GoogleTrendsComparingDesigners.csv").then(function(datapoints){
   datapoints.forEach(d => {d.H=+d.Height;});
 
-  const date = [];
-  const Lagerfeld = [];
-  const McQueen = [];
+  const Date = [];
   const Dior = [];
-  const Westwood = [];
   const Chanel = [];
+  const McQueen = [];
+  const McCartney = [];
+  const Versace = [];
 
   for (i = 0; i < datapoints.length; i++) {
-    date.push(datapoints[i].date)
-    Lagerfeld.push(datapoints[i].Lagerfeld)
-    McQueen.push(datapoints[i].McQueen)
+    Date.push(datapoints[i].date)
     Dior.push(datapoints[i].Dior)
-    Westwood.push(datapoints[i].Westwood)
     Chanel.push(datapoints[i].Chanel)
+    McQueen.push(datapoints[i].McQueen)
+    McCartney.push(datapoints[i].McCartney)
+    Versace.push(datapoints[i].Versace)
   }
 
-  const labels = date;
+  const labels = Date;
   const data = {
     labels: labels,
     datasets: [{
-      label: 'Lagerfeld',
+      label: 'Dior',
       backgroundColor: 'rgb(77, 64, 65)',
       borderColor: 'rgb(77, 64, 65)',
-      data: Lagerfeld,
-    },
-    {
-      label: 'McQueen',
-      backgroundColor: 'rgb(187, 167, 169)',
-      borderColor: 'rgb(187, 167, 169)',
-      data: McQueen,
-    },
-    {
-      label: 'Dior',
-      backgroundColor: 'rgb(226, 161, 168)',
-      borderColor: 'rgb(226, 161, 168)',
       data: Dior,
     },
     {
-      label: 'Westwood',
-      backgroundColor: 'rgb(56, 35, 37)',
-      borderColor: 'rgb(56, 35, 37)',
-      data: Westwood,
+      label: 'Chanel',
+      backgroundColor: 'rgb(187, 167, 169)',
+      borderColor: 'rgb(187, 167, 169)',
+      data: Chanel,
     },
     {
-      label: 'Chanel',
+      label: 'McQueen',
+      backgroundColor: 'rgb(226, 161, 168)',
+      borderColor: 'rgb(226, 161, 168)',
+      data: McQueen,
+    },
+    {
+      label: 'McCartney',
+      backgroundColor: 'rgb(56, 35, 37)',
+      borderColor: 'rgb(56, 35, 37)',
+      data: McCartney,
+    },
+    {
+      label: 'Versace',
       backgroundColor: 'rgb(116, 46, 53)',
       borderColor: 'rgb(116, 46, 53)',
-      data: Chanel,
+      data: Versace,
     }]
   };
 
@@ -75,50 +75,3 @@ function newDesigner(){
   console.log(newDesigner);
   document.getElementById("newDesigner").value=null;
 }
-
-function singleGraph(designerName){
-  d3.csv("GoogleTrendsComparingDesigners.csv").then(function(datapoints){
-    datapoints.forEach(d => {d.H=+d.Height;});
-  
-    const date = [];
-    const Lagerfeld = [];
-    const McQueen = [];
-    const Dior = [];
-    const Westwood = [];
-    const Chanel = [];
-  
-    for (i = 0; i < datapoints.length; i++) {
-      date.push(datapoints[i].date)
-      Lagerfeld.push(datapoints[i].Lagerfeld)
-      McQueen.push(datapoints[i].McQueen)
-      Dior.push(datapoints[i].Dior)
-      Westwood.push(datapoints[i].Westwood)
-      Chanel.push(datapoints[i].Chanel)
-    }
-  
-    const labels = date;
-    const data = {
-      labels: labels,
-      datasets: [{
-        label: designerName,
-        backgroundColor: 'rgb(77, 64, 65)',
-        borderColor: 'rgb(77, 64, 65)',
-        data: designerName,
-      }]
-    };
-  
-    const config = {
-      type: 'line',
-      data: data,
-      options: {}
-    };
-  
-    const myChart = new Chart(
-      document.getElementById(designerName),
-      config
-    );
-  
-  });
-}
-
-singleGraph(Dior)
