cfe02e56e338168f1aaf2a7cc598c1276b891e7b
LucyMauger <LucyMauger@users.noreply.github.com>
2021-12-20 01:28:07 +0000

    k

--- script.js
+++ script.js
@@ -23,9 +23,33 @@ d3.csv("GoogleTrendsComparingDesigners.csv").then(function(datapoints){
   const data = {
     labels: labels,
     datasets: [{
+      label: 'Lagerfeld',
+      backgroundColor: 'rgb(77, 64, 65)',
+      borderColor: 'rgb(77, 64, 65)',
+      data: Lagerfeld,
+    },
+    {
+      label: 'McQueen',
+      backgroundColor: 'rgb(187, 167, 169)',
+      borderColor: 'rgb(187, 167, 169)',
+      data: McQueen,
+    },
+    {
+      label: 'Dior',
+      backgroundColor: 'rgb(226, 161, 168)',
+      borderColor: 'rgb(226, 161, 168)',
+      data: Dior,
+    },
+    {
+      label: 'Westwood',
+      backgroundColor: 'rgb(56, 35, 37)',
+      borderColor: 'rgb(56, 35, 37)',
+      data: Westwood,
+    },
+    {
       label: 'My First dataset',
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgb(116, 46, 53)',
+      borderColor: 'rgb(116, 46, 53)',
       data: Chanel,
     }]
   };
